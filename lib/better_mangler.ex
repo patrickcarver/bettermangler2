c3a3f6bd384322caa45c6a772cde749a71dd236f
Patrick Carver <patrickcarver@gmail.com>
2018-11-04 00:53:03 -0500

    Remove load_json, change run to mangle

--- lib/better_mangler.ex
+++ lib/better_mangler.ex
@@ -2,19 +2,8 @@ defmodule BetterMangler do
   @moduledoc """
   BetterMangler takes an acronym and creates a fake and funny definition.
   """
-  
-  def run(acronym) do
-    list = String.codepoints(acronym)
-    nouns = load_json("../txt/nouns.json")
-    {:ok, list}
+
+  def mangle(word) do
+    {:ok, "word"}
   end
-
-  defp load_json(file) do
-    file
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> Poison.decode!()
-  end
-
-
 end

89bd66960420831c67a91a1998189be8b339198b
Patrick Carver <patrickcarver@gmail.com>
2018-11-06 23:29:40 -0600

    BetterMangler mangle gets template

--- lib/better_mangler.ex
+++ lib/better_mangler.ex
@@ -3,7 +3,19 @@ defmodule BetterMangler do
   BetterMangler takes an acronym and creates a fake and funny definition.
   """
 
+  alias BetterMangler.SentenceTemplate
+
   def mangle(word) do
-    {:ok, word}
+    letters = String.codepoints(word)
+    
+    template = get_template(letters)
+    
+    {:ok, template}
+  end
+
+  defp get_template(letters) do
+    letters
+    |> Enum.count()
+    |> SentenceTemplate.get_random_by_size()    
   end
 end

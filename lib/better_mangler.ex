10b30b52b3b1b0fd82b4dc1b44fda5a43a764727
Patrick Carver <patrickcarver@gmail.com>
2018-11-09 18:32:04 -0600

    mangle creates list of maps of letters and parts of speech

--- lib/better_mangler.ex
+++ lib/better_mangler.ex
@@ -9,13 +9,28 @@ defmodule BetterMangler do
     letters = String.codepoints(word)
     
     template = get_template(letters)
+
+    updated_template = 
+      Enum.zip(letters, template)
+      |> Enum.map(fn {letter, part_of_speech} -> %{letter: letter, part_of_speech: part_of_speech} end)
+
+
+#    updated_template = Enum.map(template, &assign_noun_plurality/1)
     
-    {:ok, template}
+    {:ok, updated_template}
   end
 
   defp get_template(letters) do
     letters
     |> Enum.count()
     |> SentenceTemplate.get_random_by_size()    
+  end
+
+  defp get_random_plurality() do
+    Enum.random([:singular, :plural])
+  end
+
+  defp get_random_tense() do
+    Enum.random([:past, :present])
   end
 end

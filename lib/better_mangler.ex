ed751680d3ebfc2904dd263f28f754c66ef4f135
Patrick Carver <patrickcarver@gmail.com>
2018-11-09 19:12:26 -0600

    Mangler applies tense to verb in template

--- lib/better_mangler.ex
+++ lib/better_mangler.ex
@@ -13,6 +13,7 @@ defmodule BetterMangler do
     updated_template = 
       Enum.zip(letters, template)
       |> Enum.map(fn {letter, part_of_speech} -> %{letter: letter, part_of_speech: part_of_speech} end)
+      |> Enum.map(&apply_tense_to_verbs/1)
 
 
 #    updated_template = Enum.map(template, &assign_noun_plurality/1)
@@ -26,11 +27,19 @@ defmodule BetterMangler do
     |> SentenceTemplate.get_random_by_size()    
   end
 
+  defp apply_tense_to_verbs(%{part_of_speech: "verb"} = map) do
+    Map.put(map, :tense, get_random_tense())
+  end
+  
+  defp apply_tense_to_verbs(map) do
+    map
+  end
+
   defp get_random_plurality() do
-    Enum.random([:singular, :plural])
+    Enum.random(["singular", "plural"])
   end
 
   defp get_random_tense() do
-    Enum.random([:past, :present])
+    Enum.random(["past", "present"])
   end
 end

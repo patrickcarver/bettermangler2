b047d20e66dfbc26ad58f13978ef28619be7155b
Patrick Carver <patrickcarver@gmail.com>
2018-11-30 01:23:17 -0600

    Merge refactor branch into master; create Word_Plurality test

--- lib/better_mangler/word_plurality.ex
+++ lib/better_mangler/word_plurality.ex
@@ -9,23 +9,19 @@ defmodule BetterMangler.WordPlurality do
   @doc """
 
   """
-  def handle_list(worded_list) when is_list(worded_list) do
+  def handle_list(worded_list) do
     Enum.map(worded_list, &handle_map/1)
   end
 
-  def handle_list({ :error, _ } = error) do
-    error
+  defp handle_map(%{part_of_speech: "noun", plurality: "plural", word: word}) do
+    Inflex.pluralize(word)
   end
 
-  defp handle_map(%{part_of_speech: "noun", plurality: "plural"} = map) do
-    Inflex.pluralize(map.word)
+  defp handle_map(%{part_of_speech: "verb", plurality: "singular", tense: "present", word: word} = map) do
+    Verbs.conjugate(word, tense: map.tense, plurality: map.plurality, person: "third")
   end
 
-  defp handle_map(%{part_of_speech: "verb", plurality: "singular", tense: "present"} = map) do
-    Verbs.conjugate(map.word, tense: "present", plurality: "singular", person: "third")
-  end
-
-  defp handle_map(map) do
-    map.word
+  defp handle_map(%{word: word}) do
+    word
   end
 end
